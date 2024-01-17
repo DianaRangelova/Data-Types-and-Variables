6ea7d7f2f658d569272c0acba3ce5ce2ea48f085
DianaRangelova <diana_kirilova@mail.bg>
2024-01-17 17:58:21 +0200

    Data Types and Variables in C#

---
+++ P06-Pets-Food/Program.cs
@@ -0,0 +1,6 @@
+﻿int countPackagesDogFood  = int.Parse(Console.ReadLine());
+int countPackagesCatFood = int.Parse(Console.ReadLine());
+
+double calculatedExpenses = countPackagesDogFood * 2.50 + countPackagesCatFood * 4.00;
+
+Console.WriteLine($"{calculatedExpenses:F2} lv.");
---
+++ P07-Projects-Creation/Program.cs
@@ -0,0 +1,6 @@
+﻿string architectsName = Console.ReadLine();
+int countOfProjects = int.Parse(Console.ReadLine());
+
+double calculateHours = countOfProjects * 3;
+
+Console.WriteLine($"The architect {architectsName} will need {calculateHours} hours to complete {countOfProjects} project/s.");
